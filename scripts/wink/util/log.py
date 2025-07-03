f07d2d6900341c196ef7d663a4564b30fe6a6c7c
jiayou <tengsong.ts@antgroup.com>
2025-07-03 20:30:27 +0800

    scripts

--- scripts/wink/util/log.py
+++ scripts/wink/util/log.py
@@ -2,18 +2,18 @@ from types import SimpleNamespace
 from typing import Protocol
 from datetime import datetime
 
-class Log(Protocol):
+class _Log(Protocol):
     def d(*args) -> None: ...
     def i(*args) -> None: ...
     def w(*args) -> None: ...
     def e(*args) -> None: ...
 
-def now():
+def _now():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
-log: Log = SimpleNamespace(
-    d = lambda *args: print(now(), 'D', *args),
-    i = lambda *args: print(now(), 'I', *args),
-    w = lambda *args: print(now(), 'W', *args),
-    e = lambda *args: print(now(), 'E', *args),
+log: _Log = SimpleNamespace(
+    d = lambda *args: print(_now(), 'D', *args),
+    i = lambda *args: print(_now(), 'I', *args),
+    w = lambda *args: print(_now(), 'W', *args),
+    e = lambda *args: print(_now(), 'E', *args),
 )
